66b0a9ab13473ad170282f3b00156c5a414ce1e0
Kota Yatagai <kota@yatagai.com>
2020-11-23 00:35:40 +0900

    Rewrite kec.rb

--- kec.rb
+++ kec.rb
@@ -1,33 +1,15 @@
-require "formula"
-
 class Kec < Formula
   desc "Image format extension converter. Only for jpg/png/gif image. Poor"
   homepage "https://github.com/kota-yata/KEC"
-  url "https://github.com/kota-yata/KEC/archive/0.0.1.tar.gz"
-  sha256 "a6dbf920948a1da8a6a65aff35ea306b31a069b1134e68a401e411f99983f7be"
+  url "https://github.com/kota-yata/KEC/releases/download/0.0.1/kec"
+  sha256 "6a7d482dc4341ff8c7b2f8cabc6ffa73937c045c6f7368538624087c5947a1ba"
   license ""
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    bin.install "kec"
   end
 
-  # test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test KEC`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    #system "false"
-  # end
+  test do
+    system "true"
+  end
 end
