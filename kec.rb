9e6651cd8cc87b00619ac618da9d5e7e4e561734
Kota Yatagai <kota@yatagai.com>
2020-11-23 17:28:57 +0900

    Rewrite formula

--- kec.rb
+++ kec.rb
@@ -1,7 +1,7 @@
 class Kec < Formula
   desc "Image format extension converter. Only for jpg/png/gif image. Poor"
-  homepage "https://github.com/kota-yata/KEC"
-  url "https://github.com/kota-yata/KEC/releases/download/0.0.1/kec"
+  homepage "https://github.com/kota-yata/kec"
+  url "https://github.com/kota-yata/kec/releases/download/0.0.1/kec"
   sha256 "6a7d482dc4341ff8c7b2f8cabc6ffa73937c045c6f7368538624087c5947a1ba"
   license ""
 
