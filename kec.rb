1eba1b299767311e7f8ddf162478d7348e5a191a
Kota Yatagai <kota@yatagai.com>
2020-11-23 19:05:09 +0900

    try gorelease

--- kec.rb
+++ kec.rb
@@ -1,9 +1,11 @@
+require 'formula'
+
 class Kec < Formula
   desc "Image format extension converter. Only for jpg/png/gif image. Poor"
-  homepage ""
+  homepage "https://github.com/kota-yata/kec"
   url "https://github.com/kota-yata/kec/archive/1.0.0.tar.gz"
   sha256 "9a569bae71ff0ce6a9fdc6e7e68a878480ed023997c5bd8ab3e54b5a22c46022"
-  license ""
+  license "MIT"
 
   def install
     bin.install "kec"
